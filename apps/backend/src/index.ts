d74c05eba5e17780279dc8e2fbe0e8757c1a6810
rushikesh <153095920+Shailesh2302@users.noreply.github.com>
2026-01-02 22:59:00 +0530

    create a components

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -41,7 +41,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3001",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     credentials: true,
   })
 );
